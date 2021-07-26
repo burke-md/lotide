ec81a273866b3c3ad75669dd70d4c59bcbb09ace
Michael <michael.dburke56@gmail.com>
2021-07-26 13:37:33 -0400

    reffactors test/middleTest.js and tests/tailTest.js with M&C

--- tests/middleTest.js
+++ tests/middleTest.js
@@ -1,7 +1,19 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1]),[]);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+
+describe('#middle', () => {
+  it("returns an empty arrray for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  // assertArraysEqual(middle([1, 2, 3]),[2]);
+
+  it("returns [2]] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
+
+// const assertArraysEqual = require('../assertArraysEqual');
+// assertArraysEqual(middle([1]),[]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
 
--- tests/tailTest.js
+++ tests/tailTest.js
@@ -1,6 +1,12 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../head');
+const assert = require('chai').assert;
+const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe('#tail', () => {
+  it("Runs function and does not alter length of array", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words); // no need to capture the return value since we are not checking it
+
+    assert.strictEqual(words.length, 3);
+  });
+});
+
